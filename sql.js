3da35a567bea5048525ed4dbfacc0b2a4106a2f6
xyloh <xylohunter1991@gmail.com>
2022-09-02 21:02:57 +0200

    firjhkl

--- sql.js
+++ sql.js
@@ -22,7 +22,7 @@ const connect=async()=>{
   }
   const insertEvent=async(data)=>{
   
-  let x = "insert into events values('"+data[1]+"','"+data[2]+"',"+data[3]+",'"+data[5]+"','"+data[0]+"','"+data[4]+"')  ";
+  let x = "insert into events values('"+data[1]+"','"+data[2]+"',"+data[3]+",'"+data[5]+"','"+data[0]+"','"+data[4]+"','"+data[6]+"')  ";
   console.log(x)
     client.query(x, (err, res) => {
       
