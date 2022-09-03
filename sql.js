4bc72f206ea6a46906695f8d2c1212f9fbc72da4
xyloh <xylohunter1991@gmail.com>
2022-09-03 20:39:18 +0200

    firjhkjkljkll

--- sql.js
+++ sql.js
@@ -4,9 +4,9 @@ const { symlinkSync } = require('fs');
 const client = new Client({
   host: 'ec2-34-234-240-121.compute-1.amazonaws.com',
   port: 5432,
-  user: 'qcnlglgoklfcdd',
-  database:'dd7oguiu2chgb6',
-  password: '8471d4b88c0932ed33f747a9f068f6166519bbf5408868cda811be2262d6023d',
+  user: 'hcrjrwqinuhycb',
+  database:'d83no383kv7p48',
+  password: '00a838892a1d712bd4c2d6625acc9bdd98c77599d9e0f26c83d84bbb2b02473c',
   ssl: { rejectUnauthorized: false }
     
 })
@@ -24,14 +24,14 @@ const connect=async()=>{
   
   let x = "insert into events values('"+data[1]+"','"+data[2]+"',"+data[3]+",'"+data[5]+"','"+data[0]+"','"+data[4]+"','"+data[6]+"')  ";
   console.log(x)
-    client.query(x, (err, res) => {
+   // client.query(x, (err, res) => {
       
-      if(err){console.log(err)}
+   //   if(err){console.log(err)}
   
      
       
    
-    })
+  //  })
   }
   const getEvents=async()=>{
     return new Promise((resolve,recive)=>{
